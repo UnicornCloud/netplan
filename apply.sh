c57dfffdf384cf630b1e2436e571c76dc530445e
Twin Flame's University <1787794+TwinFlames@users.noreply.github.com>
2020-06-09 16:05:50 -0400

    Update apply.sh

--- apply.sh
+++ apply.sh
@@ -1,4 +1,6 @@
 sudo netplan apply
 
 # Optional
-systemctl restart networking
+# https://linuxconfig.org/how-to-restart-network-on-ubuntu-20-04-lts-focal-fossa
+sudo ip link set enp0s3 down
+sudo ip link set enp0s3 up
